fa6c5840f7a3b382e2dc5b2f003b6afe81b80bad
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-07-19 15:41:56 +0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int n:nums){
+            sum+=n;
+        }
+
+        return ((nums.length + 1)*(nums.length)/2 - sum);
+    }
+}
